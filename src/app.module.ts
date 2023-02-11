02bcc727ab920cb55b59621c7441ae16ca9136df
wlrn566 <wlrn567@gmail.com>
2023-02-12 00:08:07 +0900

    환경 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,18 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TestModule } from './test/test.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, TestModule, AuthModule],
+  imports: [
+    UserModule,
+    TestModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
-  // providers: [
-  //   {
-  //     provide: APP_GUARD,
-  //     useClass: JwtAuthGuard,
-  //   },
-  //   AppService,
-  // ] // main.ts 에서 전역으로 가드를 설정하지 않고 모듈에 설정해서 종속성 주입
 })
 export class AppModule {}
