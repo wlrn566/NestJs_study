1f6bd328d9288e56a6547d767b5d559d4a7c8518
wlrn566 <wlrn567@gmail.com>
2023-02-14 23:27:54 +0900

    커밋

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ async function bootstrap() {
   // app.use(MiddlewareTest);
   // app.useGlobalGuards(new AuthGuard); 앱 전체에 가드 연결 -> 모듈에서 종속성을 주입할 수 없음 -> app.module.ts에 종속성 주입
   // app.useGlobalInterceptors(new LoggingInterceptor());
-  const configService = app.get(ConfigService);
-  const host = configService.get<string>('DB_HOST');
-  console.log(host);
+  // const configService = app.get(ConfigService);
+  // const host = configService.get<string>('DB_HOST');
+  // console.log(host);
   await app.listen(3000);
 }
 bootstrap();
